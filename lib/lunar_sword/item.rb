732450dbbbe8fb5037732044ddff098e48974204
pikesley <github@orgraphone.org>
2016-11-19 00:36:46 +0000

    Non-carriable items

--- lib/lunar_sword/item.rb
+++ lib/lunar_sword/item.rb
@@ -1,12 +1,14 @@
 module LunarSword
   class Item
-    attr_reader :id, :full_name, :thwarts, :reusable, :used
+    attr_reader :id, :full_name, :thwarts, :reusable, :used, :carriable
 
     def initialize data
+      @data = data
       @id = data['id']
       @full_name = data['full_name']
       @thwarts = data['thwarts']
       @reusable = data['reusable']
+      @carriable = data['carriable']
     end
 
     def use
